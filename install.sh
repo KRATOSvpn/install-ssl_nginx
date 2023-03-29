6ae3c6345ce7715b71de3cd012c30fa4f28091ef
oKRATOS <72270994+KRATOSvpn@users.noreply.github.com>
2023-03-28 22:36:11 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo "instalando nginx.."
 apt install nginx -y
+clear
 echo ""
-	read -p "Insira o dominio que deseja instalar o ssl: " -e -i resposta
-	read -p "Insira o proxy_pass: " -e -i proxy_pass
+echo "Insira o dominio que deseja instalar o ssl: "
+	read resposta
+    echo "Insira o proxy_pass: "
+	read proxy_pass
 echo "server {
     listen 80;
     server_name $resposta;
